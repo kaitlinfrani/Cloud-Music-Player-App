ac094c0c6590e0f25e3abe41f54105c5f2db4e57
kaitlin <kaitlinfrani@csu.fullerton.edu>
2022-10-24 00:14:05 -0700

    Set up AWS successfully

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "cloudmusicplayerapp4bb21561": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "music": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
